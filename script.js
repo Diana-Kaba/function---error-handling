2db22dbdc4b980c81cdb7d8e060c6ab7aacb661c
Diana-Kaba <kabadiana327@gmail.com>
2022-12-02 20:14:45 +0200

    second commit

--- script.js
+++ script.js
@@ -2,22 +2,28 @@
 // let expr = prompt("Введіть вираз:", "2*10+8");
 // alert(eval(expr));
 function printTable(from, to, step, func) {
-  if (func === undefined) {
-    return;
+  try {
+    if (func === undefined) {
+      return;
+    }
+    from = from;
+    to = to;
+    step = step | 1;
+    document.write("<table border='1'>");
+    for (var x = from; x <= to; x += step) {
+      document.write("<tr>");
+      document.write("<td>" + x + "</td>");
+      document.write("<td>" + func(x) + "</td>");
+      document.write("</tr>");
+    }
+    document.write("</table>");
+    if (isNaN(from, to, step)) {
+      throw new Error("Not a Number");
+    }
+  } catch (ex) {
+    alert("Извините, возникла ошибка: " + ex.message);
   }
-  from = from;
-  to = to;
-  step = step | 1;
-  document.write("<table border='1'>");
-  for (var x = from; x <= to; x += step) {
-    document.write("<tr>");
-    document.write("<td>" + x + "</td>");
-    document.write("<td>" + func(x) + "</td>");
-    document.write("</tr>");
-  }
-  document.write("</table>");
 }
-
 let f;
 
 // function quadratic(x) {
@@ -28,13 +34,7 @@ let f;
 //   return x * x * x;
 // };
 
-try {
-  let expr = prompt("Input function:");
-  if (isNaN(expr)) {
-    throw new Error("Not a Number");
-  }
-  eval("f = function(x) {return " + expr + ";}");
-  printTable(+prompt("Minimum?"), +prompt("Maximum?"), +prompt("Step"), f);
-} catch (ex) {
-  alert("Извините, возникла ошибка: " + ex.message);
-}
+let expr = prompt("Input function:");
+
+eval("f = function(x) {return " + expr + ";}");
+printTable(+prompt("Minimum?"), +prompt("Maximum?"), +prompt("Step"), f);
